24f3fd1a90b4054ad8ce3bbf746b9a85f1e7f027
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-11-16 12:14:07 -0500

    Adding imusearch tool

---
+++ imu/imuSearch.php
@@ -0,0 +1,69 @@
+<?php
+require_once "../imu-api/IMu.php";
+require_once IMu::$lib . '/Session.php';
+
+class imuSearch
+{
+
+
+  private $server;
+  private $port;
+
+  private $session;
+
+  public function __construct($serverURL, $serverPort)
+  {
+    $server = $serverURL;
+    $port = $serverPort;
+  }
+
+  public function version()
+  {
+    return IMu::VERSION
+  }
+
+  public function connect()
+  {
+    try
+    {
+      $session = new IMuSession($server, $port);
+      $session->connect();
+      return true;
+    }
+    catch (Exception $e)
+    {
+      throw $e;
+    }
+    return false;
+  }
+
+  public function search($module, $columns, $terms=null)
+  {
+    try
+    {
+      $mod = new IMuModule($module, $session);
+      if(isset($terms))
+      {
+        $search = $terms;
+        $mod->findTerms($search);
+      }
+      $result = $mod->fetch('start', 0, -1, $columns);
+      return $result;
+    }
+    catch (Exception $e)
+    {
+      throw $e;
+    }
+    return null;
+
+  }
+
+
+
+
+}
+
+
+
+
+?>
